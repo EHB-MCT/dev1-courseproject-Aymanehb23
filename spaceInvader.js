8a0eb7ef888972bc45b8b0e27cfe5d859da20a0e
ayman byd <ayman.bayoud@student.ehb.be>
2024-10-03 15:28:25 +0200

    bewerkt na advies docent

--- spaceInvader.js
+++ spaceInvader.js
@@ -13,99 +13,40 @@ let context = canvas.getContext("2d");
 tetrisLogo();
 
 
-function tetrisLogo(){
-
-context.fillStyle="Pink";
-context.beginPath();
-context.rect(186, 202, 234, 75);
-context.fill();
-context.save();
-
-context.translate(360 + 23, 264 + 75 + 37.5);
-context.rotate(Math.PI / 500);
+function tetrisLogo() {
 
 
-context.fillStyle = "pink";
     context.beginPath();
-    context.rect(-37, -100, 75, 200); // Plaats de blauwe rechthoek direct onder de roze
+    context.rect(50, 50, 300, 300);
     context.fill();
-
-    context.restore();
-context.save();
-context.translate(200 + 23, 264 + 75 + 37.5);
-context.rotate(Math.PI / 500);
+    context.closePath();
 
 
-context.fillStyle = "pink";
+
+
     context.beginPath();
-    context.rect(-38, -100, 75, 200); // Plaats de blauwe rechthoek direct onder de roze
-    context.fill();
-    context.restore();
-    context.save();
-context.translate(125 + 23, 264 + 75 + 37.5);
-context.rotate(Math.PI / 500);
-
-
-
-
-
-context.fillStyle = "pink";
-    context.beginPath();
-    context.rect(-37.5, -100, 75, 200); // Plaats de blauwe rechthoek direct onder de roze
-    context.fill();
-    context.restore();
-
-context.save();
-context.translate(435 + 23, 264 + 75 + 37.5);
-context.rotate(Math.PI / 500);
-    
-    
     context.fillStyle = "pink";
-        context.beginPath();
-        context.rect(-37.5, -100, 75, 200); // Plaats de blauwe rechthoek direct onder de roze
-        context.fill();
-        context.restore();
+    context.rect(100, 275, 200, 50);
+    context.rect(150, 250, 100, 50);
+    context.rect(100, 150, 75, 100);
+    context.rect(225, 150,75, 100);
+    context.rect(150, 100,100, 50);
     
 
-
-
-
-        context.save();
-    context.translate(150 +260,411+ 70 + 33);
-    context.rotate(Math.PI /2);
-    
-    
-    context.fillStyle = "pink";
-        context.beginPath();
-        context.rect(-38, -10, 75,239); // Plaats de blauwe rechthoek direct onder de roze
-        context.fill();
-        
-    
-       context.save();
-
-
-
-
-
-
-
-
-
-
-       context.fillStyle = "pink";
-    context.beginPath();
-    context.rect(30, 100, 75, 200); // Plaats de blauwe rechthoek direct onder de roze
     context.fill();
+    context.closePath();
+
+
     
 
-    context.restore();
 
 
 
 
-context.save();
-context.translate(100, 250, + 75 + 37.5);
-context.rotate(Math.PI / 500);
+
+
+
+
 
 
 
