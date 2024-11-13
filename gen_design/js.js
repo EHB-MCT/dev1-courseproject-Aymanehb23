c1d71f468ff5244ac929341ed427983577dc80a1
ayman byd <ayman.bayoud@student.ehb.be>
2024-11-13 21:17:24 +0100

    2 blokken gemaakt

--- gen_design/js.js
+++ gen_design/js.js
@@ -1,11 +1,36 @@
 "use strict";
-import context from "../../SCRIPT/context.js";
-import * as Utils from "../../SCRIPT/utils.js";
+
+
+
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
 
 
 drawBook();
 
-function drawBook(){
+function drawBook() {
+
+
+
+
+    context.beginPath();
+    context.rect(130, 150, 180, 300);
+    context.fill();
+    context.closePath();
+
+
+
+    context.beginPath();
+    context.rect(370, 150, 180, 300);
+
+    context.fill();
+    context.closePath();
+
+
 
 
 
